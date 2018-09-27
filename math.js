667e52e7bc0cf6db8b5fae75fa3335c60ade2948
Jack <nrickygrant@gmail.com>
2018-09-27 22:31:38 +0200

    divisible factors

--- math.js
+++ math.js
@@ -7,8 +7,12 @@ const subtractNumbers = function(a,b) {
   return a-b;
 };
 
+const divideNumbers = function(a,b){
+  return a/b;
+}
 
 console.log(addNumbers());
 
 console.log(subtractNumbers());
+console.log(divideNumbers());
 
